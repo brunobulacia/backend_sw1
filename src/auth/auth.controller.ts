11cd21a8941549b988cdc741a9b3b82517fceff8
Bruno Bulacia <beueleceia@gmail.com>
2025-08-23 00:38:12 -0400

    Public Decorator para que no pida el JWT para el endpoint de login y register, tambien subo la imagen Docker y el .dockerignore

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,17 +11,20 @@ import { AuthService } from './auth.service';
 import type { CreateUserDto } from '../users/dto/create-user.dto';
 import type { loginDto } from './dto/login.dto';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { Public } from './decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post('register')
   create(@Body() createUserDto: CreateUserDto) {
     return this.authService.create(createUserDto);
   }
 
   //AL HACER LOGIN EL USUARIO VA A RECIBIR UN ACCESS TOKEN DE TIPO BEARER PARA POSTMAN
+  @Public()
   @Post('login')
   login(@Body() loginDto: loginDto) {
     return this.authService.login(loginDto);
