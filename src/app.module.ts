1e72a3bfbe2bc4e19f37f44d6e20dd3a0ca8949b
Bruno Bulacia <beueleceia@gmail.com>
2025-08-23 00:14:26 -0400

    AUTENTICACION DE USUARIOS CON JWT SIN ROLES TODAVIA, GUARD GLOBAL PARA TODOS LOS ENDPOINTS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, AuthModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      //PARA PONER EL GUARD DE JWT EN TODOS LOS ENDPOINTS PERRITOUUUU
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
